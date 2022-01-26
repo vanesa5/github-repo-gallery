6cefb6c98b5c2b1fd280c3269cabdd5c65802583
Vanesa G <87158362+vanesa5@users.noreply.github.com>
2022-01-26 10:56:28 -0500

    Fetching user data from GitHub initiation of function.

--- js/script.js
+++ js/script.js
@@ -10,12 +10,26 @@ const username = "vanesa5";
 // ASYNC FUNCTIONS BELOW THIS LINE//
 
 const githubFetch = async function () {
-    const res = await fetch(`https://api.github.com/user${username}`); //targeted user/username with template literals (`....`)
+    const res = await fetch(`https://api.github.com/users/${username}`); //targeted user/username with template literals (`....`)
     const info = await res.json();
     console.log(info);
+
+    
+};
+    //Call Async ()
+    githubFetch();
+
+// ASYNC FUNCTIONS ABOVE THIS LINE//
+
+
+// FUNCTIONS BELOW THIS LINE//
+
+//Fetch User Data from GitHub
+const fetchUserData = function (json) {
+
 };
 
 
 
 
-// FUNCTIONS BELOW THIS LINE//
+// FUNCTIONS ABOVE THIS LINE//
