01b21affcf8f8a1d2eb005161ca1328dc2bb141a
Vanesa G <87158362+vanesa5@users.noreply.github.com>
2022-01-26 10:45:52 -0500

    troubleshoouting variables

--- js/script.js
+++ js/script.js
@@ -3,14 +3,16 @@
 // Bio section variable
 const introOverview = document.querySelector(".overview");
 //Vanesa's GitHub username
-const username = vanesa5; 
+const username = "vanesa5"; 
 
 
 
 // ASYNC FUNCTIONS BELOW THIS LINE//
 
 const githubFetch = async function () {
-    const rest = await fetch(`https://api.github.com/user${username}.`); //targeted user/username with template literals (`....`)
+    const res = await fetch(`https://api.github.com/user${username}`); //targeted user/username with template literals (`....`)
+    const info = await res.json();
+    console.log(info);
 };
 
 
