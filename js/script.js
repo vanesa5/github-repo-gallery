1d000fe20efaa92db5f1e4211c184510028bbce3
Vanesa G <87158362+vanesa5@users.noreply.github.com>
2022-01-26 10:40:54 -0500

    Async function started.

--- js/script.js
+++ js/script.js
@@ -4,3 +4,16 @@
 const introOverview = document.querySelector(".overview");
 //Vanesa's GitHub username
 const username = vanesa5; 
+
+
+
+// ASYNC FUNCTIONS BELOW THIS LINE//
+
+const githubFetch = async function () {
+    const rest = await fetch(`https://api.github.com/user${username}.`); //targeted user/username with template literals (`....`)
+};
+
+
+
+
+// FUNCTIONS BELOW THIS LINE//
